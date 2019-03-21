6173299ab7cdd2b065815c54775eb3b301e7a37f
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-03-21 14:18:33 +0530

    ADD: initialize app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { GalleryComponent } from './gallery/gallery.component';
+import { EventsComponent } from './events/events.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DarshanComponent } from './darshan/darshan.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { AudioComponent } from './audio/audio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'darshan', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  { path: 'quotes', component: QuotesComponent, data: {title: 'Quotes'}},
+  { path: 'audio', component: AudioComponent, data: {title: 'Audio'}},
+  { path: 'darshan', component: DarshanComponent, data: {title: 'Darshan'}},
+  { path: 'events', component: EventsComponent, data: {title: 'Events'}},
+  { path: 'gallery', component: GalleryComponent, data: {title: 'Gallery'}},
+  { path: '**', redirectTo: 'darshan' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { ImagePreviewDirective } from './image-preview.directive';
+
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FileUploadModule } from 'ng2-file-upload';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DarshanComponent } from './darshan/darshan.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { AudioComponent } from './audio/audio.component';
+import { FileSizePipe } from './file-size.pipe';
+import { EventsComponent } from './events/events.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GalleryComponent, GalleryModalComponent } from './gallery/gallery.component';
+import { HelperService } from './helper.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    DarshanComponent,
+    QuotesComponent,
+    AudioComponent,
+    EventsComponent,
+    ImagePreviewDirective,
+    FileSizePipe,
+    GalleryComponent,
+    GalleryModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FileUploadModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [ HelperService ],
+  entryComponents: [GalleryModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
