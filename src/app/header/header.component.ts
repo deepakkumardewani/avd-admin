2ecdc194571c9a7ee85cf4cd1ce9b03f81825ce9
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-05-25 21:33:52 +0530

    ADD: responsive title for mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { DeviceDetectorService } from 'ngx-device-detector';
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private deviceService: DeviceDetectorService
+    ) {}
 
   title: String;
+  isMobile: boolean;
+
   ngOnInit() {
+    this.isMobile = this.deviceService.isMobile();
+
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         const title = this.getTitle(
