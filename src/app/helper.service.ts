ced45fabd30a971b1503d106d547ae718b316062
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-09-02 18:42:57 +0530

    ADD: delete audio method

--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import { throwError } from 'rxjs';
+import {
+  catchError
+} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
+  allAudioUrl = `${environment.serverUrl}/lectures/audio`;
   constructor(private http: HttpClient) {}
 
    getAlbums() {
@@ -19,4 +23,25 @@ export class HelperService {
    sendNotification(body) {
      return this.http.post(`${environment.serverUrl}/notification`, body);
    }
+
+   deleteAudio(id) {
+     return this.http.delete(`${this.allAudioUrl}?id=${id}`);
+   }
+   getAllAudio(page, limit) {
+    return this.http.get<any[]>(`${this.allAudioUrl}?limit=${limit}&page=${page}`)
+    .pipe(
+       catchError(this.handleError)
+     );
+  }
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
